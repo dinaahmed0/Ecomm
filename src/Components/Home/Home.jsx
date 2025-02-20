987383e594336659d64d4fd3c9fb8427e42667d9
dinaahmed0 <dinahmed1805@gmail.com>
2025-02-20 15:58:42 +0200

    first commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import FeatureProducts from '../FeatureProducts'
-import MainSlider from '../MainSlider/MainSlider'
-import CategorySlider from '../Categoryslider/CategorysSlider'
+import React from 'react';  
+import FeatureProducts from '../FeatureProducts';  
+import MainSlider from '../MainSlider/MainSlider';  
+import CategorySlider from '../Categoryslider/CategorySlider';
 
-export default function Home() {
-  return (
-    <>
-    <MainSlider />
-    <CategorySlider />
-    <FeatureProducts />
-    </>
-  )
+export default function Home() {  
+  return (  
+    <>  
+      <MainSlider />  
+      <CategorySlider />  
+      <FeatureProducts />  
+    </>  
+  );  
 }
